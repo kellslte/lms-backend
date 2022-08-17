9a04d0d67f3620948479c19f6b09045f2dfb35b9
Favour <maxotif@gmail.com>
2022-08-17 10:45:39 +0100

    abstracted user table seeder to different file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,45 +21,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(TrackTableSeeder::class);
         $this->call(CourseTableSeeder::class);
-        if(config('app.env') === 'local'){
-            $this->createTestUsers();
-        }
-    }
-
-    public function createTestUsers()
-    {
-        Admin::create([
-            'name' => 'Favour Max-Oti',
-            'email' => 'maxotif.admin@gmail.com',
-            'recovery_email' => 'maxotif@gmail.com',
-            'password' => bcrypt('password'),
-        ])->settings()->create();
-
-        Mentor::create([
-            'name' => 'Chidera Max-Oti',
-            'email' => 'deramaxoti.mentor@gmail.com',
-            'recovery_email' => 'deramaxoti@gmail.com',
-            'password' => bcrypt('mental'),
-        ])->settings()->create();
-        
-        $course = Course::whereTitle('Cloud Engineering')->firstOrFail();
-        
-        $course->facilitator()->create([
-            'name' => 'Signs Madueke',
-            'email' => 'signs.facilitator@gmail.com',
-            'recovery_email' => 'signsmaduaeke@gmail.com',
-            'password' => bcrypt('fascille'),
-        ])->settings()->create();
-
-
-        $student =  $course->students()->create([
-            'name' => 'Amarachi Nwankwo',
-            'email' => 'amaramnwankwo@gmail.com',
-            'gender' => 'female',
-            'phonenumber' => '08106243946'
-        ]);
-
-        $student->settings()->create();
-        $student->point()->create();
+        $this->call(TestUserTableSeeder::class);
     }
 }
---
+++ database/seeders/TestUserTableSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use App\Models\Course;
+use App\Models\Mentor;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class TestUserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::create([
+            'name' => 'Favour Max-Oti',
+            'email' => 'maxotif.admin@gmail.com',
+            'recovery_email' => 'maxotif@gmail.com',
+            'password' => bcrypt('password'),
+        ])->settings()->create();
+
+        Mentor::create([
+            'name' => 'Chidera Max-Oti',
+            'email' => 'deramaxoti.mentor@gmail.com',
+            'recovery_email' => 'deramaxoti@gmail.com',
+            'password' => bcrypt('mental'),
+        ])->settings()->create();
+
+        $course = Course::whereTitle('Cloud Engineering')->firstOrFail();
+
+        $course->facilitator()->create([
+            'name' => 'Signs Madueke',
+            'email' => 'signs.facilitator@gmail.com',
+            'recovery_email' => 'signsmaduaeke@gmail.com',
+            'password' => bcrypt('fascille'),
+        ])->settings()->create();
+
+
+        $student =  $course->students()->create([
+            'name' => 'Amarachi Nwankwo',
+            'email' => 'amaramnwankwo@gmail.com',
+            'gender' => 'female',
+            'phonenumber' => '08106243946'
+        ]);
+
+        $student->settings()->create();
+        $student->point()->create();
+    }
+}
