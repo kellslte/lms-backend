67751419e3719bafa6a6894ab1cdf78469e75694
Favour <maxotif@gmail.com>
2022-08-24 09:48:04 +0100

    fixed password reset request

--- app/Services/PasswordResetService.php
+++ app/Services/PasswordResetService.php
@@ -14,19 +14,19 @@ class PasswordResetService {
         // TODO check email
         switch ($request->email) {
             case strpos($request->email, '.facilitator') !== false:
-                return (new PasswordController)->requestPasswordReset($request, "facilitator");
+                return (new PasswordController)->requestPasswordReset($request, "facilitators");
                 break;
 
             case strpos($request->email, '.admin') !== false:
-                return (new PasswordController)->requestPasswordReset($request, "admin");
+                return (new PasswordController)->requestPasswordReset($request, "admins");
                 break;
 
             case strpos($request->email, '.mentor') !== false:
-                return (new PasswordController)->requestPasswordReset($request, "mentor");
+                return (new PasswordController)->requestPasswordReset($request, "mentors");
                 break;
 
             default:
-                return (new PasswordController)->requestPasswordReset($request, "student");
+                return (new PasswordController)->requestPasswordReset($request, "users");
                 break;
         }
     }
