96e33b5b6b6e12e4828a32c28baafbd0ff79c6fe
Favour <maxotif@gmail.com>
2022-09-08 04:31:44 +0100

    added real google meet link for Sotu

--- database/seeders/MeetingsTableSeeder.php
+++ database/seeders/MeetingsTableSeeder.php
@@ -38,9 +38,9 @@ class MeetingsTableSeeder extends Seeder
 
             $data = [
                 "caption" => "State of The Union",
-                "link" => url(Str::random()),
+                "link" => "https://meet.google.com/jrc-wkag-imp",
                 "start_time" => now(),
-                "end_time" => now()->addMinutes(45),
+                "end_time" => now()->addHour(),
             ];
 
             $this->createMeeting("sotu", $data, $date);
