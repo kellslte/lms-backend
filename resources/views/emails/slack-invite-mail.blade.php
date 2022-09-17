13a906b625c61d0582077c2807a81a8985e041cc
Favour <maxotif@gmail.com>
2022-09-17 11:24:26 +0100

    fixed glitch in slakc invite email

--- resources/views/emails/slack-invite-mail.blade.php
+++ resources/views/emails/slack-invite-mail.blade.php
@@ -3,13 +3,12 @@
 
 [The ADA Programmes Team](mailto:theadaproject@enugutechhub.en.gov.ng) has invited you to use Slack with them, in the ADA Software Engineering Community. Click on the button below to join the community
 
-@component('mail::button', ['url' => {{ $link }}])
+@component('mail::button', ['url' => "{$link}"])
 Join Community
 @endcomponent
 
-### PS
-Ignore this email if you are already on the Slack community!
+### PS: Ignore this email if you are already on the Slack community!
 
-Much love,<br>
+Much love, <br>
 {{ config('app.name') }}
 @endcomponent
