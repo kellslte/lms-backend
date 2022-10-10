aaf2d5cfe9247ab7960fdccf5f2ac6dc79087b61
Favour <maxotif@gmail.com>
2022-10-10 07:58:12 +0100

    patched task controller

--- app/Http/Controllers/Facilitator/TaskController.php
+++ app/Http/Controllers/Facilitator/TaskController.php
@@ -142,6 +142,10 @@ class TaskController extends Controller
     {
         $response = TaskManager::closeTasksubmission($task);
 
+        if(!$response["message"]){
+            $response["message"] = "Task submissions closed";
+        }
+
         return response()->json([
             "message" => $response["message"],
             "task" => $response["task"],
